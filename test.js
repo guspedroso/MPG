6fb493aa8792ef18f462d449358be2314961fcdf
Gustavo Pedroso <gus@pedroso.com>
2014-03-06 23:28:43 -0600

    updated sprites.json to have all the current sprite names

--- test.js
+++ test.js
@@ -29,7 +29,7 @@ window.addEventListener("load",function() {
     init: function(p) {
 
       this._super(p,{
-        sheet:"player front"
+        sheet:"playerFaceFrontStanding"
       });
 
       this.add("2d");
