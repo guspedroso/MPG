dcea0ee6969570b9f6963ba33a7e2c7b62634208
Gustavo Pedroso <gus@pedroso.com>
2014-03-07 22:08:53 -0600

    leg movement updated

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ window.addEventListener("load",function() {
   //    to make things easy, we're going to fix this game at 640x480
   var Q = window.Q = Quintus({ development: true })
           .include("Sprites, Scenes, Input, 2D, Anim, Touch, UI")
-          .setup({ maximize: true})
+          .setup({ width: 720, height: 480})
           .controls(true)
 
   // 3. Add in the default keyboard controls
@@ -19,18 +19,18 @@ window.addEventListener("load",function() {
 	Q.gravityX = 0;
 
   Q.animations("player", {
-    fire_right_running: {frames:[10,11], rate: 1/8},
-    fire_left_running: {frames:[22,23], rate: 1/8},
-    fire_front_running: {frames:[4,5], rate: 1/8},
-    fire_back_running: {frames:[16,17], rate: 1/8},
-    fire_right_standing: {frames:[9], rate: 1/5},
-    fire_left_standing: {frames:[21], rate: 1/5},
-    fire_front_standing: {frames:[3], rate: 1/5},
-    fire_back_standing: {frames:[15], rate: 1/5},
-    run_right: {frames:[6,7], rate: 1/8},
-    run_left: {frames:[18,19], rate: 1/8},
-    run_front: {frames:[0,1], rate: 1/8},
-    run_back: {frames:[12,13], rate: 1/8},
+    fire_right_running: {frames:[10,11,9,11,10], rate: 1/10},
+    fire_left_running: {frames:[23,22,21,22,23], rate: 1/10},
+    fire_front_running: {frames:[4,5], rate: 1/10},
+    fire_back_running: {frames:[16,17], rate: 1/10},
+    fire_standing_right: {frames:[9], rate: 1/5},
+    fire_standing_left: {frames:[21], rate: 1/5},
+    fire_standing_front: {frames:[3], rate: 1/5},
+    fire_standing_back: {frames:[15], rate: 1/5},
+    run_right: {frames:[7,6,8,6,7], rate: 1/10},
+    run_left: {frames:[18,19,20,19,18], rate: 1/10},
+    run_front: {frames:[0,1], rate: 1/10},
+    run_back: {frames:[12,13], rate: 1/10},
     stand_right: {frames:[8], rate: 1/5},
     stand_left: {frames:[20], rate: 1/5},
     stand_front: {frames:[2], rate: 1/5},
@@ -52,12 +52,27 @@ window.addEventListener("load",function() {
     },
       step: function(dt) {
         if(this.p.vx > 0) {
-          this.play("run_right");
+          if (Q.inputs['action']) {
+            this.play("fire_right_running");
+          }
+          else {
+            this.play("run_right");
+          }
         } else if(this.p.vx < 0) {
-          this.play("run_left");
+          if (Q.inputs['action']) {
+            this.play("fire_left_running")
+          }
+          else {
+            this.play("run_left");
+          }
         }
         else {
-          this.play("stand_" + this.p.direction); // stand_left or stand_right
+          if (Q.inputs['action']) {
+            this.play("fire_standing_" + this.p.direction); 
+          }
+          else {
+            this.play("stand_" + this.p.direction); // stand_left or stand_right
+          }
         }
     },
   });
@@ -66,7 +81,7 @@ window.addEventListener("load",function() {
   Q.scene("level1",function(stage) {
 
     stage.insert(new Q.Repeater({ asset: "Background3.png" }));
-    var player = stage.insert(new Q.Player({ x: 48, y: 48 }));
+    var player = stage.insert(new Q.Player({ x: 400, y: 400 }));
     stage.add("viewport").follow(player);
 
   });
