ffbed9cbf83e09e04a1c1eba95668203166a6b47
Gustavo Pedroso <gus@pedroso.com>
2014-04-13 18:43:51 -0500

    update

--- client/game.js
+++ client/game.js
@@ -1321,6 +1321,7 @@ function loadCoOp() {
     
     //step function for controlling how this sprite will move
     step: function(dt) {
+      /*
       if (this.p.playerColor == "red") {
         this.p.frame = 2;
       }
@@ -1333,6 +1334,7 @@ function loadCoOp() {
       else if (this.p.playerColor == "grey") {
         this.p.frame = 52;
       }
+      */
       //Grab the input and determine which animation to play
       if(Q.inputs["right"]) {
         //set the direction of the player depending on the input
@@ -1803,18 +1805,6 @@ function loadCoOp() {
     
     //step function for controlling how this sprite will move
     animate: function(po, pf) {
-      if (this.p.playerColor == "red") {
-        this.p.frame = 2;
-      }
-      else if (this.p.playerColor == "blue") {
-        this.p.frame = 102;
-      }
-      else if (this.p.playerColor == "green") {
-        this.p.frame = 27;
-      }
-      else if (this.p.playerColor == "grey") {
-        this.p.frame = 52;
-      }
       //Grab the input and determine which animation to play
       if(po == "right") {
         //set the direction of the player depending on the input
